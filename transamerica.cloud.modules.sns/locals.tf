a78d5a52f2530e02a1e6dcae0d84caa17fe6f136
Byron Laferriere <byron.laferriere@transamerica.com>
2021-07-12 14:47:04 -0600

    commit

--- transamerica.cloud.modules.sns/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  topic_name = length(var.name) > 0 ? var.name : "${var.department}-${var.application}-${var.environment}-${var.name_suffix}"
-}
--- transamerica.cloud.modules.sns/output.tf
+++
@@ -1,9 +0,0 @@
-output "sns_topic_arn" {
-  description = "ARN of the SNS topic created"
-  value       = concat(aws_sns_topic.ta_sns_topic.*.arn, [""])[0]
-}
-
-output "subscription_id" {
-  description = "ARN of the subscription"
-  value       = aws_sns_topic_subscription.sns_notify[*].id
-}
